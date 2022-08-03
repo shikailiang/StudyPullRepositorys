a1600ed67c9b8de8f8fb7d67bcd66ef9298d16f8
skl <448022808@qq.com>
2022-08-03 13:11:54 +0800

    first commit

---
+++ app/src/main/java/com/example/aloud_cluster/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.aloud_cluster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.baijiayun.livecore.LiveSDK;
+import com.example.livemodule.Utils;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
